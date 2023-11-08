886baada360dc05f6a23fa9ea9023a9a3155b6e8
tdolan21 <timmydolan21@gmail.com>
2023-11-08 14:34:19 -0500

    reordered project page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,9 +22,8 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          <Link href="https://github.com/tdolan21?tab=repositories">
+          Code tutorials for Artificial Intelligence, Machine Learning, and Data Science          </Link>
         </div>
       </div>
     </footer>
