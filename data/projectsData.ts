afaba19fab239b9fb220c5f03043ed6ded016bbf
tdolan21 <timmydolan21@gmail.com>
2023-11-08 13:33:44 -0500

    updated blog post and projects page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'miniAGI',
-    description: `miniAGI is a Streamlit application that leverages the zero-shot ReAct Agent from Langchain with vectorsearch and many various integrations.`,
+    description: `miniAGI is a langchain application that leverages the zero-shot ReAct Agent with many popular tools and services. Works with any text-generation model on huggingface as well as OpenAI's API.`,
     imgSrc: '/static/images/miniagi.png',
     href: 'https://github.com/tdolan21/miniAGI',
   },
@@ -12,8 +12,8 @@ const projectsData = [
     href: 'https://github.com/tdolan21/zephyr-7b-alpha-api',
   },
   {
-    title: 'SSD-1B-API',
-    description: `FastAPI implementation of the SSD-1B model from the Segmind AI team.`,
+    title: 'SSD-1B + SDXL1.0 Image Gen Suite',
+    description: `Fullstack application including db and API using the SSD-1B model from the Segmind AI team as well as the SDXL1.0 model from stabilityai`,
     imgSrc: '/static/images/SSD_1B.png',
     href: 'https://github.com/tdolan21/ssd-1b-ui/tree/main',
   },
@@ -24,10 +24,16 @@ const projectsData = [
     href: 'https://github.com/tdolan21/detr-resnet-50-api',
   },
   {
-    title: 'Kosmos-2-Patch14-224 aPI',
+    title: 'Kosmos-2-Patch14-224 Demo',
     description: `This repo contains an api with postgreSQL database and a demo application for the zephyr-7b-alpha model from Huggingface.`,
     imgSrc: '/static/images/kosmos-2-thumbnail.png',
     href: 'https://github.com/tdolan21/kosmos-2-demo',
+  },
+  {
+    title: 'OpenAI-Whisper-v3 Fullstack Application',
+    description: `This repo contains an api with postgreSQL database and a demo application for the zephyr-7b-alpha model from Huggingface.`,
+    imgSrc: '/static/images/whisper-v3-thumbnail.png',
+    href: 'https://github.com/tdolan21/openai-whisper-v3-api',
   },
   
 
